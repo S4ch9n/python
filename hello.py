4943e0dde1632ef368460f72ad04fccf80653f54
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-03-02 10:53:02 +0530

    print and input function" " clear

---
+++ hello.py
@@ -0,0 +1,38 @@
+
+print("hello world!")
+print('o----')
+print(' |||| ')
+# got executed line by line
+print('*' * 10) #we can draw 10 astrick 10 time
+
+#varaible : store value temp. in computer memory
+a = 10
+print(a)#will convert to binary during execution then ot will print 10
+
+#update
+a=20
+print(a)
+print(type(a))
+
+rate = 5.4 #float value mean decimal
+print(rate)
+print(type(rate))
+
+
+name = 'John'
+print(name) #string
+print(type(name))
+
+is_true = True #boolen value
+print(is_true)
+print(type(is_true))
+
+
+#input : built-in function that are used to take input from the user
+fname = input('what is your name? ')
+print('My name is ' + fname)
+
+#question : ask person name and favourite color
+fullName = input('What is your full name ? ')
+favColor = input('What is your favourite color ? ')
+print("hello my name is " + fullName + " and my favourite color is " + favColor)
