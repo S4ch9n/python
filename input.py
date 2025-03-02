901d3818bc5c3d180db6b5b75900cfca06a2f99c
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-03-02 11:09:34 +0530

    input function in python

---
+++ input.py
@@ -0,0 +1,9 @@
+birth_year = int(input('What is your birth year ? '))
+age = 2024 - birth_year
+print(age)
+
+#ask user weight in pound and convert in into kilograms
+
+weight_lbs = float(input('weight (lbs) : '))
+weight_kgs = weight_lbs * .45
+print(weight_kgs)
