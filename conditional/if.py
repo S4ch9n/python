8c64f100ead6993a88b2d4f241cda8f8958fccba
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-03-09 21:05:21 +0530

    if condition

--- conditional/if.py
+++ conditional/if.py
@@ -8,3 +8,14 @@ elif number < 0:
     print("The number is negative.")
 else:
     print("The number is zero.")
+
+
+#eligible for vote
+# Input the age of the user
+age = int(input("Enter your age: "))
+
+# Check if the user is eligible to vote
+if age >= 18:
+    print("You are eligible to vote!")
+else:
+    print("You are not eligible to vote.")
