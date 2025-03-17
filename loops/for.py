e2fe032d40cd1bc76777f5988ec3b864e95b6c41
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-03-17 20:59:44 +0530

    for loop

--- loops/for.py
+++ loops/for.py
@@ -25,3 +25,14 @@ for digit in str(digits):
     countDigit += 1
 
 print(countDigit)
+
+
+#multiply table
+number = int(input("Enter the number to generate its multiplication table: "))
+limit = int(input("Enter the limit of the table: "))
+
+print(f"Multiplication Table for {number}:")
+
+for i in range(1, limit + 1):
+    result = number * i
+    print(f"{number} x {i} = {result}")
