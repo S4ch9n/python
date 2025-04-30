ef157a9ba10e992cf3c8b45c7c496f2472a56eaf
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-04-30 23:10:36 +0530

    for loop

--- loops/for.py
+++ loops/for.py
@@ -6,7 +6,7 @@ for i in range (1,10):
 print(sum)
 
 
-#2 1 t to 10
+#2> 1 to 10
 for i in range(1,11):
     print(i)
 
@@ -16,8 +16,54 @@ for i in range(1,11):
     print("hello")
 
 #will print odds
-for i in range(1,11,2):
-    print(i)
+for i in range(1,100,2):
+    print(f"odd numbers are : {i}")
+
+#even numbers are
+for i in range(2,101,2):
+    print(f"even numbers are : {i}")
+
+#
+#iteraton over list
+#iteraton over list
+list1 = [10,20,30,40,50]
+total = 0
+for items in list1:
+    total += items
+print(total)
+
+#nested loops
+for x in range(4):
+    for y in range(3):
+        print(f"value of x is : {x} |  value of is :  {y}  ")
+#output : 0,0
+# 0,1
+# 0,2
+# 1,0
+# 1,1
+# 1,20
+# 2,0
+# 2,1
+# 2,2
+# 3,0
+# 3,1
+# 3,2
+
+
+
+#xxxxx
+#xx
+#xxxxx
+#xx
+#xx
+numbers = [5,2,5,2,2]
+for x_count in numbers:
+    output = ""
+    for stars in range(x_count):
+        output += "x"
+    print(output)
+
+
 
 
 #count digit
