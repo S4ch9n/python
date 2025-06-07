72f9193f0c8f305867fd4700969ad57ac3fea98a
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-06-07 23:00:03 +0530

    python practice questiion

--- practice_question/questions.py
+++ practice_question/questions.py
@@ -298,3 +298,17 @@ count = 0
 for char in a:
    # Increment 'count' by 1 for each character
     count += 1
+
+
+#Python program to print even length words in a string
+s = "This is a python language"
+
+# split the sentence into words
+wrds = s.split()
+
+# filter words with even length
+even_wrds = [w for w in wrds if len(w) % 2 == 0]
+
+# Join the filtered words back into a sentence
+res = " ".join(even_wrds)
+print(res)
