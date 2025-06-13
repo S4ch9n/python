73fcf57d7ef12069751b1d2891740990c9192e5e
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-06-13 21:51:08 +0530

    exception handling

---
+++ exceptionHandling.py
@@ -0,0 +1,23 @@
+#Python Exception Handling handles errors that occur during the execution of a program.
+# Exception handling allows to respond to the error, instead of crashing the running program.
+# It enables you to catch and manage errors, making your code more robust and user-friendly. Let's look at an example:
+
+# Simple Exception Handling Example
+n = 10
+try:
+    res = n / 0  # This will raise a ZeroDivisionError
+
+except ZeroDivisionError:
+    print("Can't be divided by zero!")
+
+
+
+try:
+    n = 10
+    res = 100 / n
+except ZeroDivisionError:
+    print("Not divisible by zero")
+else:
+    print("Enter valid number")
+finally:
+    print("Execution complete")
