9deeaad72862c9f012a930421e97b62dcfc8d151
sunnyhakur666556@gmail.com <sunnythakur666556@gmail.com>
2025-07-04 19:07:45 +0530

    mdoules in python

--- modules.py
+++ modules.py
@@ -1,4 +1,13 @@
-#syntax
-import converter
+#modules = a file containing code you want to include in your program use 'import' to include a module (built-in you own useful to break
+#up a large program reusable separate files.
 
-print(converter.kg_to_lbs(70))
+import math
+print(math.pi)
+
+import math as m
+print(m.pi)
+
+from math import pi
+print(pi)
+
+
