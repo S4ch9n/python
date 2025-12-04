a4e642257b140236f7add6851364b76f2ed385bb
iam s4ch9n <sunnythakur666556@gmail.com>
2025-12-04 06:00:12 +0530

    pandas

--- PANDAS/PANDAS.PY.py
+++ PANDAS/PANDAS.PY.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from narwhals import String
+from streamlit import dataframe
 
 data = {
     "apple":  [0, 3, 4, 2],
@@ -22,3 +23,20 @@ data2 = {
 }
 df2 = pd.DataFrame(data2 , index = ["x" , "y"] , columns = ["Name" , "Name"] , dtype = String)
 print(df2)
+
+
+
+#data analysis  - > data cleaning : handling missing values , invalid data , noisy data.
+import pandas as pd
+
+data3 = {
+    "Name": ["John", "Anna", None, "Mark"],
+    "Age": [25, None, 30, "40"],
+    "City": ["NY", "LA", "NY", "LA"],
+    "Salary": ["50000", "60000", None, "70000"]
+}
+
+df3 = pd.DataFrame(data3)
+#Check basic info
+
+
